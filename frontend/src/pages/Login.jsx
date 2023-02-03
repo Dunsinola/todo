6e0d61bc8faf85892d9560fa2e9199174def4741
Dunsinola <dunsinolaleye@yahoo.com>
2023-02-03 08:28:56 +0100

    color 2

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -61,7 +61,7 @@ function Login() {
         <h1>
           <FaSignInAlt /> Login
         </h1>
-        <p>Login and start setting todos</p>
+        <p>Login and start Tracking todos</p>
       </section>
 
       <section className='form'>
